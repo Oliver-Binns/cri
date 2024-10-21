92b040034f10de7c3346e917e4d3a3ded8a835cf
Oliver Binns <mail@oliverbinns.co.uk>
2024-10-21 08:56:42 +0100

    refactor: added dot notation for claim implementations

--- Sources/Issuer/Metadata/Claims/FamilyNameClaim.swift
+++ Sources/Issuer/Metadata/Claims/FamilyNameClaim.swift
@@ -2,3 +2,9 @@ struct FamilyNameClaim: Claim {
     let name = "family_name"
     let value: [String: String] = [:]
 }
+
+extension Claim where Self == FamilyNameClaim {
+    static var familyName: Self {
+        FamilyNameClaim()
+    }
+}
